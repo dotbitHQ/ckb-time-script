d9ec2f5d4d5eed50dc6f3e313cba99d50b09286f
Link <xieaolin@gmail.com>
2021-07-15 21:10:25 +0800

    fix: ignore args verification for deploying only once for all kinds of cells

--- contracts/index-state-type/src/entry.rs
+++ contracts/index-state-type/src/entry.rs
@@ -1,12 +1,8 @@
 use crate::error::Error;
 use alloc::vec::Vec;
-use ckb_std::{
-    ckb_constants::Source,
-    ckb_types::{bytes::Bytes, packed::*, prelude::*},
-    high_level,
-};
-use core::result::Result;
+use ckb_std::{ckb_constants::Source, ckb_types::packed::*, high_level};
 use common::constants::*;
+use core::result::Result;
 
 pub fn main() -> Result<(), Error> {
     // update
@@ -19,18 +15,12 @@ pub fn main() -> Result<(), Error> {
     }
     // Create the index state cell and the input type script doesn't exist
     else {
-        load_output_type_script(|output_type_script| {
-            let out_point = high_level::load_input_out_point(0, Source::Input)?;
-            let type_args: Bytes = output_type_script.args().unpack();
-            if &type_args[..] != out_point.as_slice() {
-                return Err(Error::InvalidArgument);
-            }
+        load_output_type_script(|_| {
             let _ = check_index_state_cell_data(Source::GroupOutput)?;
             Ok(())
         })
     }
 }
-
 
 // Index state cell data: index(u8) | sum_of_time_info_cells(u8)
 fn check_index_state_cell_data(source: Source) -> Result<Vec<u8>, Error> {
@@ -55,14 +45,16 @@ fn check_index_state_cells_data() -> Result<(), Error> {
         if output_data[0] != 0 {
             return Err(Error::IndexIncreaseError);
         }
-    }
-    else if input_data[0] + 1 != output_data[0] {
+    } else if input_data[0] + 1 != output_data[0] {
         return Err(Error::IndexIncreaseError);
     }
     Ok(())
 }
 
-fn load_output_type_script<F>(closure: F) -> Result<(), Error> where F: Fn(Script) -> Result<(), Error>, {
+fn load_output_type_script<F>(closure: F) -> Result<(), Error>
+where
+    F: Fn(Script) -> Result<(), Error>,
+{
     match high_level::load_cell_type(0, Source::GroupOutput) {
         Ok(Some(output_type_script)) => closure(output_type_script),
         Ok(None) => Err(Error::IndexStateTypeNotExist),
@@ -71,21 +63,25 @@ fn load_output_type_script<F>(closure: F) -> Result<(), Error> where F: Fn(Scrip
 }
 
 fn check_cells_type_scripts_valid() -> Result<(), Error> {
-    load_output_type_script(|_| match high_level::load_cell_type(0, Source::GroupInput) {
-        Ok(Some(_)) => Ok(()),
-        Ok(None) => Err(Error::IndexStateTypeNotExist),
-        Err(_) => Err(Error::IndexStateTypeNotExist),
-    })
+    load_output_type_script(
+        |_| match high_level::load_cell_type(0, Source::GroupInput) {
+            Ok(Some(_)) => Ok(()),
+            Ok(None) => Err(Error::IndexStateTypeNotExist),
+            Err(_) => Err(Error::IndexStateTypeNotExist),
+        },
+    )
 }
 
 fn check_type_script_exists_in_inputs() -> Result<bool, Error> {
     let script = high_level::load_script()?;
-    let type_script_exists_in_inputs = high_level::QueryIter::new(high_level::load_cell_type, Source::Input).any(
-        |type_script_opt| match type_script_opt {
-            Some(type_script) => type_script.code_hash().raw_data()[..] == script.code_hash().raw_data()[..],
-            None => false,
-        },
-    );
+    let type_script_exists_in_inputs =
+        high_level::QueryIter::new(high_level::load_cell_type, Source::Input).any(
+            |type_script_opt| match type_script_opt {
+                Some(type_script) => {
+                    type_script.code_hash().raw_data()[..] == script.code_hash().raw_data()[..]
+                }
+                None => false,
+            },
+        );
     Ok(type_script_exists_in_inputs)
 }
-
